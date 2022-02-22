fb164d786bb5efd16370b96da4015ef8fe044169
thiagomrm <93530961+thiagomrm@users.noreply.github.com>
2022-02-22 20:07:40 -0300

    Tests passing - update

---
+++ src/main/java/com/devsuperior/bds02/controller/EventController.java
@@ -0,0 +1,40 @@
+package com.devsuperior.bds02.controller;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.services.EventService;
+
+@RestController
+@RequestMapping(value = "/events")
+public class EventController {
+	
+	@Autowired
+	private EventService service;
+	
+	@PostMapping
+	public ResponseEntity<EventDTO> insert(@RequestBody EventDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto){
+		dto = service.update(id, dto);	
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	
+}
